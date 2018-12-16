7a6013c0af7bf1dec08d51f77828e796d732ac69
Emmanuel Carreira <emmanuel_carreira@hotmail.com>
2018-12-15 23:49:39 -0300

    Testes com script_GA_id.m

--- Scripts/script_GA_id.m
+++ Scripts/script_GA_id.m
@@ -4,59 +4,85 @@
 %OBS: load('data.mat') tá gerando estrutura errada. Carregar data
 %diretamente dando 2 cliques no data.mat
 
+% Iniciar paralelização da CPU
+%parpool(4);
+
 % Limpar workspace
-%clc;
-%clear all;
+clc;
+clear all;
 
 global entrada saida tsaida ger
 
 % Carregar dados
-%data = load('data.mat');
-nExemplos = 100;%length(data);
-servo1 = data(1:nExemplos,1); servo2 = data(1:nExemplos,2); servo3 = data(1:nExemplos,3);
-saidaX = data(1:nExemplos,5);
-saidaY = data(1:nExemplos,6);
-saidaZ = data(1:nExemplos,7);
+load('filteredData.txt');
+data = filteredData;
+nExemplos = length(data);
+servo2 = data(1:nExemplos,4); servo3 = data(1:nExemplos,5); servo4 = data(1:nExemplos,6);
+saidaX = data(1:nExemplos,1);
+saidaY = data(1:nExemplos,2);
+saidaZ = data(1:nExemplos,3);
 
 tStop = nExemplos/10; % Tempo de simulação
-entrada = [servo1 servo2 servo3];
+entrada = [servo2 servo3 servo4];
 saida = [saidaX saidaY saidaZ];
 tsaida = 0:0.1:tStop-0.1; 
 
 % Configuração do GA
-nGeracoes = 100;
-nIndividuos = 20;
+nGeracoes = 400;
+nIndividuos = 50;
 limErro = 1e-6;
-taxaMutacao = 0.07;
-lim_inf = [-1000 -1000 -1000]; %Limite inferior
-lim_sup = [1000 1000 1000]; %Limite superior
+taxaCruzamento = 0.7;
+lim_inf = [-1000 -1000 -1000 -1000 -1000 -1000 -1000 -1000 -1000 -1000 -1000 -1000 -1000 -1000 -1000]; %Limite inferior
+lim_sup = [1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000 1000]; %Limite superior
 tam = length(lim_sup);
-options = optimoptions('ga','CrossoverFraction', taxaMutacao,'Display', 'off','FunctionTolerance', limErro,'MaxGenerations', nGeracoes*tam,'PopulationSize', nIndividuos);
+options = optimoptions('ga','CrossoverFraction', taxaCruzamento,'Display', 'off','FunctionTolerance', limErro,'MaxGenerations', nGeracoes*tam,'PopulationSize', nIndividuos);
 
 ger = 0;
 
-% Utilizando GA Toolbox
+% Executar AG usando GA Toolbox
 [ind,erro,~,output] = ga(@identificacao,tam,[],[],[],[],lim_inf,lim_sup,[],options);
 
-% Funções de transferência com individuo mais apto
-G1 = tf(1, [1, ind(1)]);
-G2 = tf(1, [1, ind(2)]);
-G3 = tf(1, [1, ind(3)]);
-XY = lft(G1,G2);
-XYZ = lft(XY,G3);
 
+% Funções de transferência com individuo mais apto
+G1 = tf(1, [1, ind(1), ind(2), ind(3), ind(4), ind(5)]);
+G2 = tf(1, [1, ind(6), ind(7), ind(8), ind(9), ind(10)]);
+G3 = tf(1, [1, ind(11), ind(12), ind(13), ind(14), ind(15)]);
+% Definição do diagrama de blocos usando Control System Toolbox
+%X = G1;
+%Y = G2;
+%Z = G3;
+XYZ = append(G1, G2, G3);
+    
 % Simular sistema com coeficientes do individuo mais apto
 saidaSimulada = lsim(XYZ,entrada,tsaida);
 
-% Plotar
+% Plotar gráficos de erro
 figure
-plot(tsaida,saida,'black',tsaida,saidaSimulada,'red')
-title('Identificação usando GA','FontSize', 16)
+plot(tsaida,saida(:,1),'black',tsaida,saidaSimulada(:,1),'red')
+title('Identificação usando GA - Erro X','FontSize', 16)
+xlabel('Tempo ','FontSize', 16)
+ylabel('Saida','FontSize', 16)
+legend('Real','Estimado')
+
+figure
+plot(tsaida,saida(:,2),'black',tsaida,saidaSimulada(:,2),'red')
+title('Identificação usando GA - Erro Y','FontSize', 16)
+xlabel('Tempo ','FontSize', 16)
+ylabel('Saida','FontSize', 16)
+legend('Real','Estimado')
+
+figure
+plot(tsaida,saida(:,3),'black',tsaida,saidaSimulada(:,3),'red')
+title('Identificação usando GA - Erro Z','FontSize', 16)
 xlabel('Tempo ','FontSize', 16)
 ylabel('Saida','FontSize', 16)
 legend('Real','Estimado')
 
 % Salvar melhor individuo
+save apto.mat ind
+
+% Finalizar paralelização
+%delete(gcp('nocreate'));
 
 % Função de aptidão
 function erro = identificacao(ind)
@@ -65,13 +91,16 @@ function erro = identificacao(ind)
     ger
     
     % Declaração das funções de tranferência
-    G1 = tf(1, [1, ind(1)]);
-    G2 = tf(1, [1, ind(2)]);
-    G3 = tf(1, [1, ind(3)]);
+    G1 = tf(1, [1, ind(1), ind(2), ind(3), ind(4), ind(5)]);
+    G2 = tf(1, [1, ind(6), ind(7), ind(8), ind(9), ind(10)]);
+    G3 = tf(1, [1, ind(11), ind(12), ind(13), ind(14), ind(15)]);
     
     % Definição do diagrama de blocos usando Control System Toolbox
-    YZ = lft(G2,G3);
-	XYZ = append(G1, G2, G3);
+    %YZ = lft(G2,G3, 1, 1);
+    %X = G1;
+    %Y = G2;
+    %Z = G3;
+    XYZ = append(G1, G2, G3);
 	
     % Simular
     saidaSimulada = lsim(XYZ, entrada, tsaida); 
@@ -83,6 +112,6 @@ function erro = identificacao(ind)
     erro = sum(raiz);
     
     % Computar erro
-    %erro = (sum((saidaSimulada-saida).^2))^(1/2);
+    %erro = sum((sum((saidaSimulada-saida).^2))^(1/2));
     ger = ger+1;
 end
